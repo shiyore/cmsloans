cb738c5b0ba329912ae3b45044f9f57eef758a67
Shiyore <shiyore@gmail.com>
2021-10-06 11:28:35 -0500

    Redo of the site

--- cmsloans/mycmsloans/mycmsloans.com/xmlrpc0db0.php
+++ cmsloans/mycmsloans/mycmsloans.com/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://mycmsloans.com</homePageLink>
+		<homePageLink>http://mycmsloans.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://mycmsloans.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://mycmsloans.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://mycmsloans.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://mycmsloans.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://mycmsloans.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://mycmsloans.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://mycmsloans.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://mycmsloans.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://mycmsloans.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://mycmsloans.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
